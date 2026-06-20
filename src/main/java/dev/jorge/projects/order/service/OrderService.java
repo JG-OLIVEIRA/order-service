eafc39d1f82a061b67d3bb1fe3edceca5e617e0d
Jorge Oliveira <74310469+JG-OLIVEIRA@users.noreply.github.com>
2026-06-20 10:47:45 -0300

    Implement synchronous communication between inventory and order service

--- src/main/java/dev/jorge/projects/order/service/OrderService.java
+++ src/main/java/dev/jorge/projects/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package dev.jorge.projects.order.service;
 
+import dev.jorge.projects.order.client.InventoryClient;
 import dev.jorge.projects.order.dto.OrderRequest;
 import dev.jorge.projects.order.model.Order;
 import dev.jorge.projects.order.repository.OrderRepository;
@@ -13,13 +14,20 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
