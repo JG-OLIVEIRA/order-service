de191b776ed31e461796a4e85304ff2844c5030f
Jorge Oliveira <74310469+JG-OLIVEIRA@users.noreply.github.com>
2026-07-08 20:23:02 -0300

    Setting up Kafka, Schema Registry, and Kafka UI using Docker Compose

--- src/main/java/dev/jorge/projects/order/dto/OrderRequest.java
+++ src/main/java/dev/jorge/projects/order/dto/OrderRequest.java
@@ -2,4 +2,6 @@ package dev.jorge.projects.order.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {}
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity, UserDetails userDetails) {
+    public record UserDetails(String email, String firstName, String lastName) {}
+}
--- src/main/java/dev/jorge/projects/order/service/OrderService.java
+++ src/main/java/dev/jorge/projects/order/service/OrderService.java
@@ -2,19 +2,26 @@ package dev.jorge.projects.order.service;
 
 import dev.jorge.projects.order.client.InventoryClient;
 import dev.jorge.projects.order.dto.OrderRequest;
+import dev.jorge.projects.order.event.OrderPlacedEvent;
 import dev.jorge.projects.order.model.Order;
 import dev.jorge.projects.order.repository.OrderRepository;
+
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -22,10 +29,15 @@ public class OrderService {
         if(isProductInStock){
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
-            order.setPrice(orderRequest.price());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
         }
